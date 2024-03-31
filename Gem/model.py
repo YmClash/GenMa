43272cbf4d7b5d73868d434f36f86313bc32b734
y_mc <momo.toure27@gmail.com>
2024-03-31 16:15:38 +0200

    add some code

--- Gem/model.py
+++ Gem/model.py
@@ -21,6 +21,7 @@ class Sampler(nn.Module):
                 top_ps: torch.Tensor,
                 tops_ks: torch.Tensor,
                 embedding_bias) -> torch.Tensor:
+
         # on choisi  le dernier element de chaque sequence
         hidden_states = hidden_states.index_select(1, output_position).squeeze(dim=1)
         logits = torch.matmul(hidden_states, embedding.t())
@@ -57,8 +58,23 @@ class Sampler(nn.Module):
         return next_token_ids
 
 
-def precompute_freqs_ids(dim: int, end: int, theta: float = 10000.0) -> torch.Tensor:
-    pass
+def precompute_freqs_ids(dim: int,
+                         end: int,
+                         theta: float = 10000.0) -> torch.Tensor:
+    freqs = 1.0 / (theta**(torch.arange(0,dim,2)[:(dim // 2)].float() / dim))
+    t =  torch.arange(end,device=freqs.device)
+    freqs = torch.outer(t,freqs).float()
+    freqs_cis = torch.polar(torch.ones_like(freqs),freqs) # complex 64
+    return freqs_cis
 
-def apply_rotary_emb():
-    pass
+
+def apply_rotary_emb(x:torch.Tensor,freqs_cis: torch.Tensor) -> torch.Tensor:
+    x_ = torch.view_as_complex(
+        torch.stack(torch.chunk(x.transpose(1,2).float(),2,dim=-1),dim=-1))
+
+    x_out = torch.view_as_real(x_ * freqs_cis).type_as(x)
+    x_out = torch.cat(torch.chunk(x_out, 2, dim=-1), dim=-2)
+    x_out = x_out.reshape(x_out.shape[0], x_out.shape[1],x_out.shape[2],-1).transpose(1,2)
+
+    return x_out
+
