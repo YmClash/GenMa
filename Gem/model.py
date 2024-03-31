b39b7b49c99ead0ad1b9ef231fe6b1e606722113
y_mc <momo.toure27@gmail.com>
2024-03-31 19:12:24 +0200

    add some code

--- Gem/model.py
+++ Gem/model.py
@@ -78,3 +78,30 @@ def apply_rotary_emb(x:torch.Tensor,freqs_cis: torch.Tensor) -> torch.Tensor:
 
     return x_out
 
+
+class Linear(nn.Module):
+    def __init__(self,in_features:int,out_features: int,quant:bool):
+        super().__init__()
+        if quant:
+            self.weight = nn.Parameter(
+                torch.empty((out_features,in_features),dtype=torch.int8),
+                requires_grad=False
+            )
+            self.weight_scaler = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.weight = nn.Parameter(
+                torch.empty((out_features,in_features)),
+                requires_grad=False
+            )
+        self.quant = quant
+
+    def forward(self,x):
+        weight = self.weight
+        if self.quant:
+            weight = weight * self.weight_scaler.unsqueeze(-1)
+        output = F.linear(x,weight)
+        return output
+
+
+
+
